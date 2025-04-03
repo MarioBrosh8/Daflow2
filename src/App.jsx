33ef8f334a39389c5c7c02904a1e3e2faafff403
MarioBrosh <valdezmario6@gmail.com>
2025-04-02 19:20:15 -0600

    Add header functionality and image collection

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import './custom.scss'
 import { HeroSection } from './components/HeroSection'
 import { Container } from 'react-bootstrap'
 import { Header } from './components/Header';
+import { MenuOverlay } from './components/MenuOverlay'
+import { ImagesCollection } from './components/ImagesCollection';
 import { useState } from 'react';
 
 function App() {
@@ -13,7 +15,9 @@ function App() {
     <div className="App">
       <Container>
         <Header navbarOpen={navbarOpen} setNavbarOpen={setNavbarOpen}/>
+        <MenuOverlay navbarOpen={navbarOpen} setNavbarOpen={setNavbarOpen}/>
         <HeroSection />
+        <ImagesCollection />
       </Container>
     </div>
   )
---
+++ src/components/ImagesCollection.jsx
@@ -0,0 +1,20 @@
+
+export const ImagesCollection = () => {
+    const imagesList = [...new Array(9)].map((number, index) => `/images/image-${index + 1}.jpg`)
+    return (
+        <ul className="images-collection-container">
+            {imagesList.map((imgUrl, index) => {
+                return (
+                    <li className="item">
+                        <figure>
+                            <img src={imgUrl} alt="collection" />
+                        </figure>
+                    </li>
+                )
+            }
+            )
+
+            }
+        </ul>
+    )
+}
